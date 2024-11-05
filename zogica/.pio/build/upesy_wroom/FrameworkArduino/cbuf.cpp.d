2f84f4a90b9dab595836425beabed097312e8ba0
leonbl <leon.blazic@gmail.com>
2024-11-05 14:39:12 +0100

    zogica

---
+++ zogica/.pio/build/upesy_wroom/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/upesy_wroom/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/b35/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/b35/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ zogica/.pio/build/upesy_wroom/lib016/Adafruit GFX Library/glcdfont.c.d
@@ -0,0 +1,2 @@
+.pio/build/upesy_wroom/lib016/Adafruit\ GFX\ Library/glcdfont.c.o: \
+ .pio/libdeps/upesy_wroom/Adafruit\ GFX\ Library/glcdfont.c
