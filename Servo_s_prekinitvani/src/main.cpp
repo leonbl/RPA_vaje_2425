a6c86a9418d22b23bf4790a69134ac7b875f12c3
leonbl <leon.blazic@gmail.com>
2024-11-29 09:12:03 +0100

    Update main.cpp

--- Servo_s_prekinitvani/src/main.cpp
+++ Servo_s_prekinitvani/src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 #define led 11
 
+HardwareTimer *Timer2 = new HardwareTimer(TIM2);
 int32_t pos = 0, predznak = 10;
-void setServo(uint32_t pos);
 void timerInt();
+void timer2Int();
 
 void setup() {
   pinMode(9, OUTPUT);
@@ -12,26 +13,22 @@ void setup() {
   Timer1->setOverflow(20000, MICROSEC_FORMAT);
   Timer1->attachInterrupt(timerInt);
   Timer1->resume();
+  Timer2->attachInterrupt(timer2Int);
   Serial.begin(9600);
 }
 
 void loop() {
-  pos = pos + predznak;
-  if(pos>1000){
-    predznak = -10;
-  }
-  if(pos<0){
-    predznak = 10;
-  }
-  delay(10);
-}
-
-void setServo(uint32_t pos){
-  digitalWrite(9, 1);
-  delayMicroseconds(1000 + pos);
-  digitalWrite(9, 0);
+  
 }
 
 void timerInt(){
-  setServo(pos);
+  digitalWrite(9, 1);
+  Timer2->setOverflow(pos + 1000, MICROSEC_FORMAT);
+  Timer2->refresh();
+  Timer2->resume();
+}
+
+void timer2Int(){
+  digitalWrite(9, 0);
+  Timer2->pause();
 }
